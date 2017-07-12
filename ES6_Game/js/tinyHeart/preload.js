dfb864bf74da4d84d24986fb424b9abc7b0cde9d
Pinzi123 <995537534@qq.com>
2017-07-12 20:12:41 +0800

    22

--- ES6_Game/js/tinyHeart/preload.js
+++ ES6_Game/js/tinyHeart/preload.js
@@ -1,6 +1,3 @@
-let imgs = ["baby.png","babyEye0.png","babyEye1.png","babyFade0.png","babyFade1.png","babyFade10.png","babyFade11.png","babyFade12.png","babyFade13.png","babyFade14.png","babyFade15.png","babyFade16.png","babyFade17.png","babyFade18.png","babyFade19.png","babyFade2.png","babyFade3.png","babyFade4.png","babyFade5.png","babyFade6.png","babyFade7.png","babyFade8.png","babyFade9.png","babyTail0.png","babyTail1.png","babyTail2.png","babyTail3.png","babyTail4.png","babyTail5.png","babyTail6.png","babyTail7.png","background.jpg","big.png","bigEat0.png","bigEat1.png","bigEat2.png","bigEat3.png","bigEat4.png","bigEat5.png","bigEat6.png","bigEat7.png","bigEatBlue0.png","bigEatBlue1.png","bigEatBlue2.png","bigEatBlue3.png","bigEatBlue4.png","bigEatBlue5.png","bigEatBlue6.png","bigEatBlue7.png","bigEye0.png","bigEye1.png","bigSwim0.png","bigSwim1.png","bigSwim2.png","bigSwim3.png","bigSwim4.png","bigSwim5.png","bigSwim6.png","bigSwim7.png","bigSwimBlue0.png","bigSwimBlue1.png","bigSwimBlue2.png","bigSwimBlue3.png","bigSwimBlue4.png","bigSwimBlue5.png","bigSwimBlue6.png","bigSwimBlue7.png","bigTail0.png","bigTail1.png","bigTail2.png","bigTail3.png","bigTail4.png","bigTail5.png","bigTail6.png","bigTail7.png","blue.png","cover.png","dust0.png","dust1.png","dust2.png","dust3.png","dust4.png","dust5.png","dust6.png","fruit.png","play.png"]
-console.log(imgs.length);
-
 class PreLoad {
   constructor (imgs, each = null, all = null) {
     this.imgs = (typeof imgs === 'string') ? [imgs] : imgs
@@ -29,22 +26,7 @@ class PreLoad {
       }
     })
   }
-
-  eachSrc(src) {
-
-  }
 }
 
-let pro = document.getElementById("progress")
-let length = imgs.length
-let preLoad = new PreLoad(imgs, function(count){
-                                  pro.innerHTML = Math.round((count+1)/length * 100) + '%'
-                                },
-                                function(){
-                                  console.log('over');
-                                  pro.style.display = "none"
-                                  document.getElementById('allcanvas').style.display = "block"
-                                  var tiny = new tinyHeart()
-                                  document.body.onload = tiny.game()
-                                })
-preLoad._unorderes()
+
+module.exports = PreLoad
--- dist/js/tinyHeart.js
+++ dist/js/tinyHeart.js
@@ -76,6 +76,10 @@
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
+var _preload = __webpack_require__(20);
+
+var _preload2 = _interopRequireDefault(_preload);
+
 var _ane = __webpack_require__(5);
 
 var _ane2 = _interopRequireDefault(_ane);
@@ -117,7 +121,6 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 __webpack_require__(14);
-__webpack_require__(15);
 
 var tinyHeart = function () {
   function tinyHeart() {
@@ -214,7 +217,19 @@ var tinyHeart = function () {
 }();
 
 var tiny = new tinyHeart();
-document.body.onload = tiny.game();
+var imgs = ["baby.png", "babyEye0.png", "babyEye1.png", "babyFade0.png", "babyFade1.png", "babyFade10.png", "babyFade11.png", "babyFade12.png", "babyFade13.png", "babyFade14.png", "babyFade15.png", "babyFade16.png", "babyFade17.png", "babyFade18.png", "babyFade19.png", "babyFade2.png", "babyFade3.png", "babyFade4.png", "babyFade5.png", "babyFade6.png", "babyFade7.png", "babyFade8.png", "babyFade9.png", "babyTail0.png", "babyTail1.png", "babyTail2.png", "babyTail3.png", "babyTail4.png", "babyTail5.png", "babyTail6.png", "babyTail7.png", "background.jpg", "big.png", "bigEat0.png", "bigEat1.png", "bigEat2.png", "bigEat3.png", "bigEat4.png", "bigEat5.png", "bigEat6.png", "bigEat7.png", "bigEatBlue0.png", "bigEatBlue1.png", "bigEatBlue2.png", "bigEatBlue3.png", "bigEatBlue4.png", "bigEatBlue5.png", "bigEatBlue6.png", "bigEatBlue7.png", "bigEye0.png", "bigEye1.png", "bigSwim0.png", "bigSwim1.png", "bigSwim2.png", "bigSwim3.png", "bigSwim4.png", "bigSwim5.png", "bigSwim6.png", "bigSwim7.png", "bigSwimBlue0.png", "bigSwimBlue1.png", "bigSwimBlue2.png", "bigSwimBlue3.png", "bigSwimBlue4.png", "bigSwimBlue5.png", "bigSwimBlue6.png", "bigSwimBlue7.png", "bigTail0.png", "bigTail1.png", "bigTail2.png", "bigTail3.png", "bigTail4.png", "bigTail5.png", "bigTail6.png", "bigTail7.png", "blue.png", "cover.png", "dust0.png", "dust1.png", "dust2.png", "dust3.png", "dust4.png", "dust5.png", "dust6.png", "fruit.png", "play.png"];
+console.log(imgs.length);
+var pro = document.getElementById("progress");
+var length = imgs.length;
+var preLoad = new _preload2.default(imgs, function (count) {
+  pro.innerHTML = Math.round((count + 1) / length * 100) + '%';
+}, function () {
+  console.log('over');
+  pro.style.display = "none";
+  document.getElementById('allcanvas').style.display = "block";
+  tiny.game();
+});
+preLoad._unorderes();
 
 /***/ }),
 /* 5 */
@@ -947,7 +962,12 @@ module.exports = dustObj;
 // removed by extract-text-webpack-plugin
 
 /***/ }),
-/* 15 */
+/* 15 */,
+/* 16 */,
+/* 17 */,
+/* 18 */,
+/* 19 */,
+/* 20 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -956,9 +976,6 @@ module.exports = dustObj;
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var imgs = ["baby.png", "babyEye0.png", "babyEye1.png", "babyFade0.png", "babyFade1.png", "babyFade10.png", "babyFade11.png", "babyFade12.png", "babyFade13.png", "babyFade14.png", "babyFade15.png", "babyFade16.png", "babyFade17.png", "babyFade18.png", "babyFade19.png", "babyFade2.png", "babyFade3.png", "babyFade4.png", "babyFade5.png", "babyFade6.png", "babyFade7.png", "babyFade8.png", "babyFade9.png", "babyTail0.png", "babyTail1.png", "babyTail2.png", "babyTail3.png", "babyTail4.png", "babyTail5.png", "babyTail6.png", "babyTail7.png", "background.jpg", "big.png", "bigEat0.png", "bigEat1.png", "bigEat2.png", "bigEat3.png", "bigEat4.png", "bigEat5.png", "bigEat6.png", "bigEat7.png", "bigEatBlue0.png", "bigEatBlue1.png", "bigEatBlue2.png", "bigEatBlue3.png", "bigEatBlue4.png", "bigEatBlue5.png", "bigEatBlue6.png", "bigEatBlue7.png", "bigEye0.png", "bigEye1.png", "bigSwim0.png", "bigSwim1.png", "bigSwim2.png", "bigSwim3.png", "bigSwim4.png", "bigSwim5.png", "bigSwim6.png", "bigSwim7.png", "bigSwimBlue0.png", "bigSwimBlue1.png", "bigSwimBlue2.png", "bigSwimBlue3.png", "bigSwimBlue4.png", "bigSwimBlue5.png", "bigSwimBlue6.png", "bigSwimBlue7.png", "bigTail0.png", "bigTail1.png", "bigTail2.png", "bigTail3.png", "bigTail4.png", "bigTail5.png", "bigTail6.png", "bigTail7.png", "blue.png", "cover.png", "dust0.png", "dust1.png", "dust2.png", "dust3.png", "dust4.png", "dust5.png", "dust6.png", "fruit.png", "play.png"];
-console.log(imgs.length);
 
 var PreLoad = function () {
   function PreLoad(imgs) {
@@ -973,7 +990,7 @@ var PreLoad = function () {
   }
 
   _createClass(PreLoad, [{
-    key: "_unorderes",
+    key: '_unorderes',
     value: function _unorderes() {
       var count = 0;
       var each = this.each;
@@ -995,26 +1012,12 @@ var PreLoad = function () {
         };
       });
     }
-  }, {
-    key: "eachSrc",
-    value: function eachSrc(src) {}
   }]);
 
   return PreLoad;
 }();
 
-var pro = document.getElementById("progress");
-var length = imgs.length;
-var preLoad = new PreLoad(imgs, function (count) {
-  pro.innerHTML = Math.round((count + 1) / length * 100) + '%';
-}, function () {
-  console.log('over');
-  pro.style.display = "none";
-  document.getElementById('allcanvas').style.display = "block";
-  var tiny = new tinyHeart();
-  document.body.onload = tiny.game();
-});
-preLoad._unorderes();
+module.exports = PreLoad;
 
 /***/ })
 /******/ ]);
